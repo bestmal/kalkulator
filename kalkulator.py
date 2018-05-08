30c2b27a3001946cfef619607cab1ec1bfccfb43
Malgorzata <malgorzatabeszterda1@wp.pl>
2018-05-08 16:49:21 +0200

    Dodano odejmowanie

--- kalkulator.py
+++ kalkulator.py
@@ -5,10 +5,15 @@ print ('to jest kalkulator sumowania')
 def sumuj(liczba1, liczba2):
     return liczba1 + liczba2
 
+def odejmowanie(liczba1, liczba2):
+    return liczba1 - liczba2
+
 
 a = int(input("Podaj liczbe A: "))
 b = int(input("Podaj liczbe B: "))
 
 print()
 wynik = sumuj(a, b)
+wynik2 = odejmowanie(a, b)
 print("%s + %s = %s" % (a, b, wynik))
+print("%s - %s = %s" % (a, b, wynik2))
