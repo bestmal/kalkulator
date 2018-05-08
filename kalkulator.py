410dc7fd0a86d72a7d4e57a2f41597d80a4a94e1
Malgorzata <malgorzatabeszterda1@wp.pl>
2018-05-08 16:47:25 +0200

    Dodano sumowanie

--- kalkulator.py
+++ kalkulator.py
@@ -1,2 +1,14 @@
 print('hello world')
-print ('to jest kalkulator')
+print ('to jest kalkulator sumowania')
+
+
+def sumuj(liczba1, liczba2):
+    return liczba1 + liczba2
+
+
+a = int(input("Podaj liczbe A: "))
+b = int(input("Podaj liczbe B: "))
+
+print()
+wynik = sumuj(a, b)
+print("%s + %s = %s" % (a, b, wynik))
